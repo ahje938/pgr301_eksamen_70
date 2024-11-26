9a9bdda62fc0705c68cd9e500bfb4c9b1fb38c24
ahje938 <anders-1221@hotmail.com>
2024-11-26 02:57:24 +0000

    fikset feil med main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -19,13 +19,13 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# sqs kø
+# SQS Queue
 resource "aws_sqs_queue" "image_processing_queue_70" {
   name                        = "image-processing-queue-70"
   visibility_timeout_seconds  = 60
 }
 
-# iam rolle
+# IAM Role for Lambda
 resource "aws_iam_role" "lambda_role_eksamen70" {
   name = "lambda-sqs-role-eksamen70"
   assume_role_policy = jsonencode({
@@ -42,7 +42,7 @@ resource "aws_iam_role" "lambda_role_eksamen70" {
   })
 }
 
-# policy for lambda
+# IAM Policy for Lambda
 resource "aws_iam_policy" "lambda_sqs_policy_eksamen70" {
   name = "lambda-sqs-policy-eksamen70"
   policy = jsonencode({
@@ -68,7 +68,7 @@ resource "aws_iam_policy" "lambda_sqs_policy_eksamen70" {
       {
         Effect = "Allow"
         Action = "logs:*"
-        Resource = "*"
+        Resource = "arn:aws:logs:*:*:*"
       },
       {
         Effect = "Allow"
@@ -77,21 +77,30 @@ resource "aws_iam_policy" "lambda_sqs_policy_eksamen70" {
       },
       {
         Effect = "Allow"
-        Action = "kms:Decrypt"
-        Resource = "*"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PassRole",
+          "iam:GetRole",
+          "iam:ListRolePolicies",
+          "iam:GetRolePolicy"
+        ]
+        Resource = "arn:aws:iam::*:role/*"
       }
     ]
   })
 }
 
-# policy til rolle
+
+# Attach Policy to Role
 resource "aws_iam_role_policy_attachment" "attach_policy_eksamen70" {
   role       = aws_iam_role.lambda_role_eksamen70.name
   policy_arn = aws_iam_policy.lambda_sqs_policy_eksamen70.arn
 }
 
-
-# lambda funksjonen
+# Lambda Function
 resource "aws_lambda_function" "lambda_sqs_processor_eksamen70" {
   function_name = "sqs-image-processor-eksamen70"
   runtime       = "python3.9"
@@ -112,7 +121,7 @@ resource "aws_lambda_function" "lambda_sqs_processor_eksamen70" {
   depends_on = [aws_iam_role_policy_attachment.attach_policy_eksamen70]
 }
 
-# sqs > lambda
+# Event Source Mapping
 resource "aws_lambda_event_source_mapping" "sqs_to_lambda_eksamen70" {
   event_source_arn = aws_sqs_queue.image_processing_queue_70.arn
   function_name    = aws_lambda_function.lambda_sqs_processor_eksamen70.arn
